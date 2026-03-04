a38f83f4e8c640a64f9400a97936f4f68c6be61a
Shuo Wang <shawnwang.tech@gmail.com>
2026-03-04 13:05:29 +0800

    Use relative path for directory change in compile script for improved portability

--- thesis_final/compile.sh
+++ thesis_final/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /Users/wangshuo/Documents/workspace/phd_thesis_latex/thesis_final && latexmk -xelatex -g wangshuo_phdthesis.tex
+cd "$(dirname "$0")" && latexmk -xelatex -g wangshuo_phdthesis.tex
